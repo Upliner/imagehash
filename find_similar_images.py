9cbcdb9b893ca3fc3f98d460a03d7198aac9d5f9
Michael Vigovsky <upliner@gmail.com>
2017-05-14 01:14:15 +0300

    find_similiar_images: use multiple hashes to compare images

--- find_similar_images.py
+++ find_similar_images.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 from __future__ import (absolute_import, division, print_function)
 from PIL import Image
-import six, imagehash, os
+import imagehash, os
 
 import imagehash
 
+hashfuncs = [imagehash.average_hash, imagehash.phash, imagehash.dhash, imagehash.whash, lambda img: imagehash.whash(img, mode='db4')]
+
 verbose = True
+
 """
 Demo of hashing
 """
-def find_similar_images(userpath, hashfunc = imagehash.average_hash):
+def find_similar_images(userpath):
 
     def is_image(filename):
         f = filename.lower()
@@ -23,48 +26,57 @@ def find_similar_images(userpath, hashfunc = imagehash.average_hash):
                     yield tup[0]+"/"+f
 
     image_filenames = walk(userpath)
-    images = {}
     for img in image_filenames:
-        hash = hashfunc(Image.open(img))
+        if img in imgfiles:
+            continue
+        hl = []
+        for i in range(len(hashfuncs)):
+            hash = str(hashfuncs[i](Image.open(img)))
+            hl += [hash]
+            imghashes[i][hash] = imghashes[i].get(hash, []) + [img]
+        imgfiles[img] = hl
         if verbose:
-             print(img + " " + str(hash))
-        images[hash] = images.get(hash, []) + [img]
+            print(img)
+        hashfile.write(img + "\t" + "\t".join(hl) + "\n")
+        hashfile.flush()
 
-    for k, img_list in six.iteritems(images):
-        if len(img_list) > 1:
-            print(" ".join(img_list))
+    while len(imgfiles) > 0:
+        img, hashes = imgfiles.popitem()
+        imgs = {img: True}
+        for i in range(len(hashfuncs)):
+            for nimg in imghashes[i][hashes[i]]:
+                imgs[nimg] = True
+                imgfiles.pop(nimg, None)
+        if len(imgs) > 1:
+            print("\t".join(imgs.keys()))
 
 
 if __name__ == '__main__':
     import sys
     def usage():
-        sys.stderr.write("""SYNOPSIS: %s [ahash|phash|dhash|...] [<directory>]
+        sys.stderr.write("""SYNOPSIS: %s [<directory>]
 
 Identifies similar images in the directory.
 
-Method: 
-  ahash:      Average hash
-  phash:      Perceptual hash
-  dhash:      Difference hash
-  whash-haar: Haar wavelet hash
-  whash-db4:  Daubechies wavelet hash
-
 (C) Johannes Buchner, 2013-2017
+(C) Michael Vigovsky, 2017
 """ % sys.argv[0])
         sys.exit(1)
 
-    hashmethod = sys.argv[1] if len(sys.argv) > 1 else usage()
-    if hashmethod == 'ahash':
-        hashfunc = imagehash.average_hash
-    elif hashmethod == 'phash':
-        hashfunc = imagehash.phash
-    elif hashmethod == 'dhash':
-        hashfunc = imagehash.dhash
-    elif hashmethod == 'whash-haar':
-        hashfunc = imagehash.whash
-    elif hashmethod == 'whash-db4':
-        hashfunc = lambda img: imagehash.whash(img, mode='db4')
-    else:
-        usage()
-    userpath = sys.argv[2] if len(sys.argv) > 2 else "."
-    find_similar_images(userpath=userpath, hashfunc=hashfunc)
+    imghashes = [{} for i in hashfuncs]
+    imgfiles = {}
+    try:
+        with open("hashes.txt","r") as f:
+            for line in f:
+                hl = line.rstrip("\n").split("\t")
+                if len(hl) != len(hashfuncs)+1:
+                    continue
+                imgfiles[hl[0]] = hl[1:]
+                for i in range(len(hashfuncs)):
+                    imghashes[i][hl[i+1]] = imghashes[i].get(hl[i+1],[]) + [hl[0]]
+    except:
+        pass
+    hashfile = open("hashes.txt","a")
+
+    userpath = sys.argv[1] if len(sys.argv) > 1 else "."
+    find_similar_images(userpath=userpath)
